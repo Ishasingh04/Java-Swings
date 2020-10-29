3923dabb5d88f47c17bae6c0368cf35f5bef5846
Isha <54667057+Ishasingh04@users.noreply.github.com>
2020-10-30 00:10:52 +0530

    ButtonIcon
    Adding an image on the button

---
+++ ButtonIcon.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+public class ButtonIcon {
+	ButtonIcon(){
+		JFrame f = new JFrame("ButtonIcon");
+		JButton b = new JButton(new ImageIcon("button.png"));
+		b.setBounds(80,150,150,40);
+		b.setText("Button");
+		f.add(b);
+		f.setSize(300,400);
+		f.setLayout(null);
+		f.setVisible(true);
+		
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new ButtonIcon();
+	}
+
+}
--- HelloThere.java
+++ HelloThere.java
@@ -1,12 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class JFrame1 {
+public class HelloThere {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JFrame f = new JFrame();
-		JButton b = new JButton("Click Here");
+		JButton b = new JButton(new ImageIcon("button.jpg"));
 		b.setBounds(50,100,95,30);
 		JTextField tf = new JTextField();
 		tf.setBounds(50,50,150,20);
