ee68ba70e6dd700026c1a6348def038e31120e2d
Isha <54667057+Ishasingh04@users.noreply.github.com>
2020-11-11 22:17:11 +0530

    edited!
    modified a button

--- Java Swings Programs/HelloThere.java
+++ Java Swings Programs/HelloThere.java
@@ -6,7 +6,7 @@ public class HelloThere {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JFrame f = new JFrame();
-		JButton b = new JButton("click");
+		JButton b = new JButton(new ImageIcon("../button.jpg"));
 		b.setBounds(50,100,95,30);
 		JTextField tf = new JTextField();
 		tf.setBounds(50,50,150,20);
